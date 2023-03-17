2e1c2f9f0dc6f96ec2bf623ba3f0ec0f57b20552
Kerry Snyder <kerry@atmanaut.me>
2023-03-17 13:06:50 -0700

    content: add why to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import TemplateStart from '$lib/comps/TemplateStart.svelte';
 	import ElementGrid from '$lib/comps/ElementGrid.svelte';
+	import Why from '$lib/comps/Why.svelte';
 </script>
 
 <TemplateStart />
 <ElementGrid />
+<Why />
